d55f5f07591e8b74da9e7bb712fd555a01fbea30
MarcosIgnacioo <magma_ruby145@hotmail.com>
2024-09-21 17:54:27 -0700

    nyaaaaaa

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,6 @@
 import { Button } from "@/components/ui/button"
 
 function App() {
-
-
   return (
     <Button>POPO</Button >
   )
--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,9 @@ import { defineConfig } from "vite"
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist'
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
